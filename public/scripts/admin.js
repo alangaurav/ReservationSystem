a034846bda69424591715e4e03cbb950ae4d12c1
Alan Gaurav <alangaurav@outlook.com>
2020-10-17 20:49:54 +0530

    [Frontend] scripts: Update the scripts.
    Both user and admin scripts now have
    separate functions to render different
    DOM elements. Ajax calls are now processed
    when required.
    
    Signed-off-by: Alan Gaurav <alangaurav@outlook.com>

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -1,80 +1,145 @@
-$(document).ready(function() {
+var user;
+
+$(document).ready(function () {
+    getUserInfo();
+    renderDeviceTable();
+});
+
+function getUserInfo() {
+    $.ajax({
+        type: "GET",
+        url: '/current-user',
+        dataType: "json",
+        success: function (result) {
+            user = result.uid;
+            $('.title-user').append(result.name + '.');
+        }
+    });
+}
+
+function renderDeviceTable() {
     $.ajax({
         type: "GET",
         url: '/device-data',
         dataType: "json",
-        success: function(result) {
-            renderDeviceTable(result);
+        success: function (data) {
+            $('.table-body__device').html('');
+            data.forEach(element => {
+                if (element.currentUser == null) {
+                    $('.table-body__device').append(
+                        '<tr class="table-body-row">' +
+                        '<td class="table-body-element">' + element.did + '</td>' +
+                        '<td class="table-body-element">' + element.name + '</td>' +
+                        '<td class="table-body-element">' + element.pcip + '</td>' +
+                        '<td class="table-body-element"></td>' +
+                        '<td class="table-body-element"><button class="button button-delete" id="' + element.did + '">delete</button>&nbsp<a class="button button-update" id="' + element.did + '" href="#popup">update</a></td>' +
+                        '</tr>'
+                    )
+                }
+                else {
+                    $('.table-body__device').append(
+                        '<tr class="table-body-row">' +
+                        '<td class="table-body-element">' + element.did + '</td>' +
+                        '<td class="table-body-element">' + element.name + '</td>' +
+                        '<td class="table-body-element">' + element.pcip + '</td>' +
+                        '<td class="table-body-element">' + element.currentUser + '</td>' +
+                        '</tr>'
+                    )
+                }
+            })
         }
-      });
+    });
+}
 
-      $.ajax({
+function renderUserTable() {
+    $.ajax({
         type: "GET",
         url: '/user-data',
         dataType: "json",
-        success: function(result) {
-            renderUserTable(result);
+        success: function (data) {
+            $('.table-body__user').html('');
+            data.forEach(element => {
+                $('.table-body__user').append(
+                    '<tr class="table-body-row">' +
+                    '<td class="table-body-element">' + element.uid + '</td>' +
+                    '<td class="table-body-element">' + element.name + '</td>' +
+                    '<td class="table-body-element">' + element.uip + '</td>' +
+                    '<td class="table-body-element"> <button class="button button-delete" id="' + element.uid + '">delete</button></td>' +
+                    '</tr>'
+                )
+            })
         }
-      });
+    });
+}
 
-      $.ajax({
+function renderPCTable() {
+    $.ajax({
         type: "GET",
         url: '/pc-data',
         dataType: "json",
-        success: function(result) {
-            renderPCTable(result);
+        success: function (data) {
+            $('.table-body__pc').html('');
+            data.forEach(element => {
+                if (element.currentUser == null) {
+                    $('.table-body__pc').append(
+                        '<tr class="table-body-row">' +
+                        '<td class="table-body-element">' + element.name + '</td>' +
+                        '<td class="table-body-element">' + element.ip + '</td>' +
+                        '<td class="table-body-element"></td>' +
+                        '<td class="table-body-element"><button class="button button-delete" id="' + element.ip + '">delete</button>&nbsp<a class="button button-update" id="' + element.ip + '" href="#popup">update</a></td>' +
+                        '</tr>'
+                    )
+                }
+                else {
+                    $('.table-body__pc').append(
+                        '<tr class="table-body-row">' +
+                        '<td class="table-body-element">' + element.name + '</td>' +
+                        '<td class="table-body-element">' + element.ip + '</td>' +
+                        '<td class="table-body-element">' + element.currentUser + '</td>' +
+                        '</tr>'
+                    )
+                }
+            })
         }
-      });
-});
-
-function renderDeviceTable(data) {
-    data.forEach(element => {
-        $('.table-body__device').append(
-            '<tr class="table-body-row">' +
-            '<td class="table-body-element">' + element.did + '</td>' +
-            '<td class="table-body-element">' + element.name + '</td>' +
-            '<td class="table-body-element">' + element.pcip + '</td>' +
-            '<td class="table-body-element">' + element.currentUser + '</td>' +
-            '<td class="table-body-element"> <button class="button button-delete" id="' + element.did + '">delete</button></td>' +
-            '</tr>'
-        )
-    })
+    });
 }
 
-function renderUserTable(data) {
-    data.forEach(element => {
-        $('.table-body__user').append(
-            '<tr class="table-body-row">' +
-            '<td class="table-body-element">' + element.uid + '</td>' +
-            '<td class="table-body-element">' + element.name + '</td>' +
-            '<td class="table-body-element">' + element.uip + '</td>' +
-            '<td class="table-body-element"> <button class="button button-delete" id="' + element.uid + '">delete</button></td>' +
-            '</tr>'
-        )
-    })
-}
+$('.sidenav-list').on('click', '.sidenav-list-item', function () {
+    var clickedItem = $(this).attr('value');
+    if (clickedItem == 'devices')
+        renderDeviceTable();
+    else if (clickedItem == 'pcs')
+        renderPCTable();
+    else if (clickedItem == 'users')
+        renderUserTable();
 
-function renderPCTable(data) {
-    data.forEach(element => {
-        $('.table-body__pc').append(
-            '<tr class="table-body-row">' +
-            '<td class="table-body-element">' + element.name + '</td>' +
-            '<td class="table-body-element">' + element.ip + '</td>' +
-            '<td class="table-body-element"> <button class="button button-delete" id="' + element.ip + '">delete</button></td>' +
-            '</tr>'
-        )
+    $('.sidenav-list > li').each(function () {
+        $(this).removeClass('sidenav-list-item__active');
     })
-}
+
+    $(this).addClass('sidenav-list-item__active');
+
+    $('.main-container > section').each(function () {
+        if ($(this).attr('id') == clickedItem) {
+            $(this).addClass('active');
+            $(this).removeClass('inactive');
+        }
+        else {
+            $(this).addClass('inactive');
+            $(this).removeClass('active');
+        }
+    })
+})
 
 $('.table-body__device').on('click', '.button-delete', function () {
     var deviceId = $(this).attr('id');
-    var obj = { 'deviceId': deviceId};
+    var obj = { 'deviceId': deviceId };
     $.ajax({
         type: "DELETE",
         url: "/delete-device",
         data: obj,
         dataType: "text",
-        success: function(){
+        success: function () {
             alert("You have successfully deleted the device " + deviceId + "!");
             location.reload();
         },
@@ -83,13 +148,13 @@ $('.table-body__device').on('click', '.button-delete', function () {
 
 $('.table-body__user').on('click', '.button-delete', function () {
     var uid = $(this).attr('id');
-    var obj = { 'uid': uid};
+    var obj = { 'uid': uid };
     $.ajax({
         type: "DELETE",
         url: "/delete-user",
         data: obj,
         dataType: "text",
-        success: function(){
+        success: function () {
             alert("You have successfully deleted " + uid + "!");
             location.reload();
         },
@@ -98,24 +163,50 @@ $('.table-body__user').on('click', '.button-delete', function () {
 
 $('.table-body__pc').on('click', '.button-delete', function () {
     var pcip = $(this).attr('id');
-    var obj = { 'pcip': pcip};
+    var obj = { 'pcip': pcip };
     $.ajax({
         type: "DELETE",
         url: "/delete-pc",
         data: obj,
         dataType: "text",
-        success: function(){
+        success: function () {
             alert("You have successfully deleted the pc " + pcip + "!");
             location.reload();
         },
     })
 })
 
-$('#logout').on('click', function() {
+$('.table-body__device').on('click', '.button-update', function () {
+    var row = $(this).closest('tr');
+    var name = row.children('td').eq(1).html();
+    var ip = row.children('td').eq(2).html();
+    var id = $(this).attr('id');
+
+    $('.popup__content > .form').attr('action', '/update-device');
+
+    var idGroup = $('.form-group.inactive');
+    console.log(idGroup);
+    idGroup.removeClass('inactive');
+    $('.form-input__id').val(id);
+    $('.form-input__name').val(name);
+    $('.form-input__ip').val(ip);
+})
+
+$('.table-body__pc').on('click', '.button-update', function () {
+    var row = $(this).closest('tr');
+    var name = row.children('td').eq(0).html();
+    var ip = $(this).attr('id');
+
+    $('.popup__content > .form').attr('action', '/update-pc');
+    $('.form-input__name').val(name);
+    $('.form-input__ip').val(ip);
+})
+
+$('#signout').on('click', function () {
     $.ajax({
         type: "GET",
         url: "/logout",
-        success: function(){
+        success: function () {
             alert("You have successfully logged out!");
             location.reload();
         },
--- public/scripts/user.js
+++ public/scripts/user.js
@@ -7,14 +7,15 @@ $(document).ready(function () {
         url: '/current-user',
         dataType: "json",
         success: function (result) {
-            user = result.user;
+            user = result.uid;
+            $('.title-user').append(result.name + '.');
 
             $.ajax({
                 type: "GET",
                 url: '/device-data',
                 dataType: "json",
                 success: function (result) {
-                    renderTable(result);
+                    renderDeviceTable(result);
                 }
             });
         }
@@ -22,41 +23,136 @@ $(document).ready(function () {
 
 });
 
-function renderTable(data) {
-    data.forEach(element => {
-        if (element.inUse == true) {
-            if (element.currentUser == user) {
-                $('.table-body').append(
-                    '<tr class="table-body-row">' +
-                    '<td class="table-body-element">' + element.did + '</td>' +
-                    '<td class="table-body-element">' + element.name + '</td>' +
-                    '<td class="table-body-element">' + element.pcip + '</td>' +
-                    '<td class="table-body-element">' + element.currentUser + '</td>' +
-                    '<td class="table-body-element"> <button class="button button-delete" id="' + element.did + '">delete </button></td>' +
-                    '</tr>'
-                )
-            }
-            else {
-                $('.table-body').append(
-                    '<tr class="table-body-row">' +
-                    '<td class="table-body-element">' + element.did + '</td>' +
-                    '<td class="table-body-element">' + element.name + '</td>' +
-                    '<td class="table-body-element">' + element.pcip + '</td>' +
-                    '<td class="table-body-element">' + element.currentUser + '</td>' +
-                    '</tr>'
-                )
-            }
-        }
-        else {
-            $('#device-select').append(
-                '<option value="' + element.did + '">' + element.name + ' - ' + element.did + '</option>'
-            )
+function renderDeviceTable() {
+    $.ajax({
+        type: "GET",
+        url: '/device-data',
+        dataType: "json",
+        success: function (data) {
+            $('#table-device .table-body').html('');
+            data.forEach(element => {
+                if (element.currentUser == user) {
+                    $('#table-device .table-body').append(
+                        '<tr class="table-body-row">' +
+                        '<td class="table-body-element">' + element.did + '</td>' +
+                        '<td class="table-body-element">' + element.name + '</td>' +
+                        '<td class="table-body-element">' + element.pcip + '</td>' +
+                        '<td class="table-body-element">' + element.currentUser + '</td>' +
+                        '<td class="table-body-element"> <button class="button button-delete" id="' + element.did + '">delete </button></td>' +
+                        '</tr>'
+                    )
+                }
+                else if (element.currentUser == null) {
+                    $('#table-device .table-body').append(
+                        '<tr class="table-body-row">' +
+                        '<td class="table-body-element">' + element.did + '</td>' +
+                        '<td class="table-body-element">' + element.name + '</td>' +
+                        '<td class="table-body-element">' + element.pcip + '</td>' +
+                        '<td class="table-body-element"></td>' +
+                        '<td class="table-body-element"> <button class="button button-reserve" id="' + element.did + '">reserve </button></td>' +
+                        '</tr>'
+                    )
+                }
+                else {
+                    $('#table-device .table-body').append(
+                        '<tr class="table-body-row">' +
+                        '<td class="table-body-element">' + element.did + '</td>' +
+                        '<td class="table-body-element">' + element.name + '</td>' +
+                        '<td class="table-body-element">' + element.pcip + '</td>' +
+                        '<td class="table-body-element">' + element.currentUser + '</td>' +
+                        '</tr>'
+                    )
+                }
+            })
         }
     })
 }
 
-$('#reserve').click(function () {
-    var deviceId = $('#device-select option:selected').val();
+function renderPCTable() {
+    $.ajax({
+        type: "GET",
+        url: '/pc-data',
+        dataType: "json",
+        success: function (data) {
+            $('#table-pc .table-body').html('');
+            data.forEach(element => {
+                if (element.currentUser == user) {
+                    $('#table-pc .table-body').append(
+                        '<tr class="table-body-row">' +
+                        '<td class="table-body-element">' + element.name + '</td>' +
+                        '<td class="table-body-element">' + element.ip + '</td>' +
+                        '<td class="table-body-element">' + element.currentUser + '</td>' +
+                        '<td class="table-body-element"> <button class="button button-delete" id="' + element.ip + '">delete </button></td>' +
+                        '</tr>'
+                    )
+                }
+                else if (element.currentUser == null) {
+                    $('#table-pc .table-body').append(
+                        '<tr class="table-body-row">' +
+                        '<td class="table-body-element">' + element.name + '</td>' +
+                        '<td class="table-body-element">' + element.ip + '</td>' +
+                        '<td class="table-body-element"></td>' +
+                        '<td class="table-body-element"> <button class="button button-reserve" id="' + element.ip + '">reserve </button></td>' +
+                        '</tr>'
+                    )
+                }
+                else {
+                    $('#table-pc .table-body').append(
+                        '<tr class="table-body-row">' +
+                        '<td class="table-body-element">' + element.name + '</td>' +
+                        '<td class="table-body-element">' + element.ip + '</td>' +
+                        '<td class="table-body-element">' + element.currentUser + '</td>' +
+                        '</tr>'
+                    )
+                }
+            })
+        }
+    })
+}
+
+function renderUserInfo() {
+    console.log("user info called");
+    $.ajax({
+        type: "GET",
+        url: '/current-user',
+        dataType: "json",
+        success: function (result) {
+            $('#uid').val(result.uid);
+            $('#uname').val(result.name);
+            $('#uIP').val(result.uip);
+        }
+    })
+}
+
+$('.sidenav-list').on('click', '.sidenav-list-item', function () {
+    var clickedItem = $(this).attr('value');
+    if (clickedItem == 'devices')
+        renderDeviceTable();
+    else if (clickedItem == 'pcs')
+        renderPCTable();
+    else if (clickedItem == 'user-info')
+        renderUserInfo();
+
+    $('.sidenav-list > li').each(function () {
+        $(this).removeClass('sidenav-list-item__active');
+    })
+
+    $(this).addClass('sidenav-list-item__active');
+
+    $('.main-container > section').each(function () {
+        if ($(this).attr('id') == clickedItem) {
+            $(this).addClass('active');
+            $(this).removeClass('inactive');
+        }
+        else {
+            $(this).addClass('inactive');
+            $(this).removeClass('active');
+        }
+    })
+})
+
+$('#table-device .table-body').on('click', '.button-reserve', function () {
+    var deviceId = $(this).attr('id');
     var obj = { 'deviceId': deviceId };
     $.ajax({
         type: "POST",
@@ -65,12 +161,12 @@ $('#reserve').click(function () {
         dataType: "text",
         success: function () {
             alert("You have successfully reserved the device" + deviceId + "!");
-            location.reload();
+            renderDeviceTable();
         },
     })
 })
 
-$('table').on('click', '.button-delete', function () {
+$('#table-device .table-body').on('click', '.button-delete', function () {
     var deviceId = $(this).attr('id');
     var obj = { 'deviceId': deviceId };
     $.ajax({
@@ -79,13 +175,43 @@ $('table').on('click', '.button-delete', function () {
         data: obj,
         dataType: "text",
         success: function () {
-            alert("You have successfully removed the device" + deviceId + "!");
-            location.reload();
+            alert("You have successfully removed the device " + deviceId + "!");
+            renderDeviceTable();
         },
     })
 })
 
-$('#logout').on('click', function () {
+$('#table-pc .table-body').on('click', '.button-reserve', function () {
+    var pcip = $(this).attr('id');
+    var obj = { 'pcip': pcip };
+    $.ajax({
+        type: "POST",
+        url: "/reserve-pc",
+        data: obj,
+        dataType: "text",
+        success: function () {
+            alert("You have successfully reserved the pc " + pcip + "!");
+            renderPCTable();
+        },
+    })
+})
+
+$('#table-pc .table-body').on('click', '.button-delete', function () {
+    var pcip = $(this).attr('id');
+    var obj = { 'pcip': pcip };
+    $.ajax({
+        type: "POST",
+        url: "/remove-pc",
+        data: obj,
+        dataType: "text",
+        success: function () {
+            alert("You have successfully removed the pc " + pcip + "!");
+            renderPCTable();
+        },
+    })
+})
+
+$('#signout').on('click', function () {
     $.ajax({
         type: "GET",
         url: "/logout",
